51c4f1c6604731154ebbcb85e38166c9f4bbb67f
VIKRANT SINGH <63734627+VIKRANT17565@users.noreply.github.com>
2023-10-17 11:28:33 +0530

    Add files via upload

---
+++ graph_generator.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from pymongo import MongoClient
+import matplotlib.pyplot as plt
+
+def backend():
+    client = MongoClient("mongodb+srv://rblfinalproject:automated141633@automatedcluster.yegmx7e.mongodb.net/") 
+    db = client["SIS"]  
+    collection = db["SDP"]  
+    cursor = collection.find({})
+    return cursor
+
+def sdp_graphs():
+    cursor=backend()
+    '''Attendance Graph'''
+    df = pd.DataFrame(list(cursor))
+    df1=df.groupby('branch_div')['attendace'].sum()
+    df1.plot(kind='bar', figsize=(10, 5), title='Branch-wise Attendance', color='pink')
+    plt.savefig(r'static\sdp_attendance.png')
+
+    '''Category Graph'''
+    df1=df['category'].value_counts()
+    df1.plot(kind='bar', figsize=(10, 5), title='Category Distribution',color='pink')
+    plt.savefig(r'static\sdp_category.png')
+    print('Done')
+
+def act_graphs():
+    cursor=backend()
+    '''Attendance Graph'''
+    df = pd.DataFrame(list(cursor))
+    df1=df.groupby('branch_div')['attendace'].sum()
+    df1.plot(kind='bar', figsize=(10, 5), title='Branch-wise Attendance', color='blue')
+    plt.savefig(r'static\act_attendance.png')
+
+    '''Category Graph'''
+    df1=df['category'].value_counts()
+    df1.plot(kind='bar', figsize=(10, 5), title='Category Distribution',color='blue')
+    plt.savefig(r'static\act_category.png')
+    print('Done')
+
+backend()
+if __name__ == '__main__':
+    sdp_graphs()
+    act_graphs()
